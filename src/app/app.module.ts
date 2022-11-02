a7dfbb0d65f73260317ea0eed3ee9bfe8dbd6210
94925 <nagendrareddy9913@gmail.com>
2022-11-02 11:19:06 +0530

    applied bootstrap to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  
+    LoginComponent,
+    DashboardComponent,
+    PagenotfoundComponent,
+
     
   ],
   imports: [

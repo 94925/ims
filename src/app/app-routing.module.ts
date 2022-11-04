7af7802711e044170d8de10faf06563e8217992d
94925 <nagendrareddy9913@gmail.com>
2022-11-04 12:24:10 +0530

    Dashboard implemented completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllstudentsComponent } from './allstudents/allstudents.component';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { CreatestudentComponent } from './createstudent/createstudent.component';
 import { AuthGuard } from './guards/auth.guard';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [ 
   {path: 'login',component:LoginComponent},
-  {path: 'dashboard',component:DashboardComponent,canActivate:[AuthGuard]},
+  {path: 'dashboard',component:DashboardComponent,canActivate:[AuthGuard],children:[
+    {path: 'createstudent',component:CreatestudentComponent},
+    {path: 'allstudents',component:AllstudentsComponent}
+
+  ]},
   {path: '',component:LoginComponent},
   {path: '**',component:PagenotfoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HttpClientModule} from '@angular/common/http';
+import { CreatestudentComponent } from './createstudent/createstudent.component';
+import { AllstudentsComponent } from './allstudents/allstudents.component';
 
 
 @NgModule({
@@ -16,6 +17,9 @@ import { HttpClientModule} from '@angular/common/http';
     LoginComponent,
     DashboardComponent,
     PagenotfoundComponent,
+    CreatestudentComponent,
+    AllstudentsComponent,
+  
 
     
   ],
